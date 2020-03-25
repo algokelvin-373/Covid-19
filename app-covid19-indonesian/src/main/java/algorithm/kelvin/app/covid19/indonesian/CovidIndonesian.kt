beec5bb7cd2263b2e12851ac0c3f1e0784517116
kelvin373-ht <kelvin373.ht@gmail.com>
2020-03-25 16:26:35 +0700

    Add CovidIndonesian Page Activity

---
+++ app-covid19-indonesian/src/main/java/algorithm/kelvin/app/covid19/indonesian/CovidIndonesian.kt
@@ -0,0 +1,12 @@
+package algorithm.kelvin.app.covid19.indonesian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CovidIndonesian : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_covid_indonesian)
+    }
+}
