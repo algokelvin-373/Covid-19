1f1c0c34e1dc5bb8643b8e75c02d3fe418dde54c
kelvin373-ht <kelvin373.ht@gmail.com>
2020-03-30 23:19:56 +0700

    Add Method COnstantVal and RecyclerView

---
+++ utils/src/main/java/algorithm/kelvin/utils/recyclerview/DataDiffUtil.kt
@@ -0,0 +1,11 @@
+package algorithm.kelvin.utils.recyclerview
+
+import android.annotation.SuppressLint
+import androidx.recyclerview.widget.DiffUtil
+
+class DataDiffUtil<T> : DiffUtil.ItemCallback<T>() {
+    override fun areItemsTheSame(oldItem: T, newItem: T): Boolean = oldItem == newItem
+
+    @SuppressLint("DiffUtilEquals")
+    override fun areContentsTheSame(oldItem: T, newItem: T): Boolean = oldItem == newItem
+}
