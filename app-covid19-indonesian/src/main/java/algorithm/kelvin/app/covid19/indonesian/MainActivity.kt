5dfe6cc3aa3551627d1274454840c8a137c45111
kelvin373-ht <kelvin373.ht@gmail.com>
2020-03-31 20:17:06 +0700

    Repair MainActivity Page

--- app-covid19-indonesian/src/main/java/algorithm/kelvin/app/covid19/indonesian/MainActivity.kt
+++ app-covid19-indonesian/src/main/java/algorithm/kelvin/app/covid19/indonesian/MainActivity.kt
@@ -1,12 +1,16 @@
 package algorithm.kelvin.app.covid19.indonesian
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_covid_19_indonesian.setOnClickListener { startActivity(Intent(this, CovidIndonesian::class.java)) }
     }
 }
